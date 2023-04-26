a008085cd6f893ebf56c3184ca6b88475baf2d01
RodATS <77297145+RodATS@users.noreply.github.com>
2023-04-26 10:28:31 -0500

    Update main.py

--- Ciudades_Generaciones/main.py
+++ Ciudades_Generaciones/main.py
@@ -42,14 +42,11 @@ def cambioCaminos(caminos,bits):
   indexUno = 1
   indexCero = 1
   while(indexCero < limite and indexUno < limite):
-
     if(indexCero == indexUno and bits[indexCero][0] == 1 and bits[indexUno][0] == 1):
         indexCero += 1
         indexUno += 1
-
     elif(indexCero <= indexUno and bits[indexCero][0] == 0 and bits[indexUno][0] == 0):
       indexUno +=1
-
     elif(indexCero < indexUno and bits[indexCero][0] == 0 and bits[indexUno][0] == 1):
       temp = caminos[indexCero]
       caminos[indexCero] = caminos[indexUno]
@@ -63,6 +60,8 @@ def cambioCaminos(caminos,bits):
 def encontrarLosCaminosMasCaros(costoCamino,generacionN):
     indiceMaximo1 = 0
     indiceMaximo2 = 0
+    indiceMinimo3 = 0
+    indiceMinimo4 = 0
 
     for i in range(1,len(costoCamino)):
         if( costoCamino[i][generacionN] > costoCamino[indiceMaximo1][generacionN]):
@@ -70,10 +69,19 @@ def encontrarLosCaminosMasCaros(costoCamino,generacionN):
     
 
     for j in range(1,len(costoCamino)):
-        if( costoCamino[j][generacionN] > costoCamino[indiceMaximo2][generacionN] and costoCamino[j][generacionN] < costoCamino[indiceMaximo1][generacionN]):
+        if( costoCamino[j][generacionN] > costoCamino[indiceMaximo2][generacionN] and costoCamino[j][generacionN] < costoCamino[indiceMaximo1][generacionN] and indiceMaximo1 != j):
             indiceMaximo2 = j
+
+    for i in range(1,len(costoCamino)):
+        if( costoCamino[i][generacionN] < costoCamino[indiceMinimo3][generacionN] and i != indiceMaximo1 and i != indiceMaximo2 ):
+            indiceMinimo3 = i
+    
+
+    for j in range(1,len(costoCamino)):
+        if( costoCamino[j][generacionN] < costoCamino[indiceMinimo4][generacionN] and costoCamino[j][generacionN] > costoCamino[indiceMinimo3][generacionN] and j != indiceMinimo3 ):
+            indiceMinimo4 = j
         
-    return indiceMaximo1,indiceMaximo2
+    return indiceMaximo1,indiceMaximo2,indiceMinimo3, indiceMinimo4
 
 
 
@@ -89,10 +97,11 @@ def encontrarCaminoMinimo(costoCamino,generacionN):
 
 
 
+
 nCiudades = 20
 #A B C D E 
 #Hay 13
-posCiudades = [[2,3],[3,4],[5,4],[5,2],[3,1],[6,1],[1,1],[2,1],[3,3],[1,5],[4,2],[9,1],[2,9],[5,5],[8,9],[3,2],[6,7],[7,3],[6,6],[7,2]]
+posCiudades = [[2,3],[3,4],[5,4],[5,2],[3,1],[6,1],[1,1],[2,1],[3,3],[1,5],[4,2],[9,1],[2,9],[5,5],[1,9],[3,2],[6,7],[7,3],[6,6],[7,2]]
 
 
 nGeneraciones = 100
@@ -131,10 +140,10 @@ promedios = []
 costoPorCamino = [[],[],[],[]]
 
 
-
+posMinIncio = 0
 #generaciones = 10
 for gen in range(nGeneraciones):
-    #print("generacion " + str(gen))
+    print("generacion " + str(gen))
     prom = 0
     for i in range(len(caminos)):
         distancia = 0
@@ -145,27 +154,40 @@ for gen in range(nGeneraciones):
         #print("\n")
         costoPorCamino[i].append(distancia)
         prom += distancia
-    promedios.append(prom/len(caminos))
+    promedios.append(prom/nCaminos)
 
     #seleccionar los caminos mas costosos
     #posiciones de los caminos mas caros, inicilizacion
-    pos1,pos2 = encontrarLosCaminosMasCaros(costoPorCamino,gen)
+    pos1,pos2, pos3, pos4 = encontrarLosCaminosMasCaros(costoPorCamino,gen)
+
+    #posiciones de los caminos mas baratos, inicilizacion
+
+    #print(costoPorCamino)
+    #hacer la copia
+    #caminos[pos1] = caminos[pos3]
+    #caminos[pos2] = caminos[pos4]
 
     #print(pos1)
     #print(pos2)
+    #print(pos3)
+    #print(pos4)
 
     #print(costoPorCamino)
-    # cambio en los caminos mas caros
+    # cambio en los caminos mas caros (copias)
+    print(bitsCambio)
     cambioCaminos(caminos[pos1],bitsCambio)
     cambioCaminos(caminos[pos2],bitsCambio)
 
     cambioBitsPorGeneracion(bitsCambio)
+    if(gen == 0):
+        posMinIncio = encontrarCaminoMinimo(costoPorCamino,gen)
     
 
-posMin = encontrarCaminoMinimo(costoPorCamino,nGeneraciones-1)
+posMinFinal = encontrarCaminoMinimo(costoPorCamino,nGeneraciones-1)
 
 print("Debería graficar pronto")
 
 plt.plot(ejexGeneraciones,promedios, ":",color="b")
-plt.plot(ejexGeneraciones, costoPorCamino[posMin],":",color="r")
+plt.plot(ejexGeneraciones, costoPorCamino[posMinIncio],":",color="r")
+plt.plot(ejexGeneraciones, costoPorCamino[posMinFinal],":",color="g")
 plt.show()
